caf5826e6f6089e20ad186402a5b7bb3ea7b1d41
realkanavarora <arora.kanav777@gmail.com>
2022-10-28 12:07:35 +0530

    changed aos duration

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,7 +94,7 @@
    */
   window.addEventListener('load', () => {
     AOS.init({
-      duration: 1000,
+      duration: 2000,
       easing: 'ease-in-out',
       once: true,
       mirror: false
